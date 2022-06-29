4239bef58f075a1f404cc0969e20eb8b5326c857
^ↀᴥↀ^ <87715226+Xenos-134@users.noreply.github.com>
2022-06-29 17:10:09 +0100

    Addded: Recusive function to read All images

--- py/hands_data_extraction.py
+++ py/hands_data_extraction.py
@@ -2,6 +2,7 @@
 
 #TODO <make method to generate diferent out file type>
 
+from asyncore import write
 from pickle import TRUE
 import sys
 import os
@@ -19,10 +20,11 @@ DIR_PATH = "./"
 IS_SINGLE_FILE = False
 OUTPUT_NAME = "out.txt"
 DEFAULT_OUT_TYPE = "txt"
+SHOW_PROCESSED_IMAGE = False
+SHOW_CONSOLE_LOG = False
 
 
-
-def get_landmarks(dir_name, is_single = IS_SINGLE_FILE, show_processed_image = False, out_file_name = OUTPUT_NAME):
+def get_landmarks(dir_name, output_file, flags):
     with mp_hands.Hands(
         static_image_mode=True,
         max_num_hands=1,
@@ -30,6 +32,7 @@ def get_landmarks(dir_name, is_single = IS_SINGLE_FILE, show_processed_image = F
         IMAGE_FILES = list(filter(lambda file_name: '.png' in file_name or '.jpg' in file_name, os.listdir(dir_name)))
         #list(filter(lambda file_name: '.png' in file_name or '.jpg' in file_name, os.listdir(dir_name)))
         print(list(filter(lambda file_name: '.png' in file_name or '.jpg' in file_name, IMAGE_FILES)))
+        
         for idx, file in enumerate(IMAGE_FILES):
 
             image = cv2.flip(cv2.imread(dir_name+'/'+file), 1)
@@ -43,12 +46,14 @@ def get_landmarks(dir_name, is_single = IS_SINGLE_FILE, show_processed_image = F
             image_height, image_width, _ = image.shape
             annotated_image = image.copy()
             for hand_landmarks in results.multi_hand_landmarks:
-                print('hand_landmarks:', hand_landmarks)
-                print(
-                    f'Index finger tip coordinates: (',
-                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-                    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-                )
+                if(flags['show_console_log']):
+                    print('hand_landmarks:', hand_landmarks)
+                    print(
+                        f'Index finger tip coordinates: (',
+                        f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+                        f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+                    )
+                save_landmarks(hand_landmarks, output_file)
             mp_drawing.draw_landmarks(
                 annotated_image,
                 hand_landmarks,
@@ -57,7 +62,7 @@ def get_landmarks(dir_name, is_single = IS_SINGLE_FILE, show_processed_image = F
                 mp_drawing_styles.get_default_hand_connections_style())
 
             #IF WE WANT TO SEE PROCESSED IAMGE OUTPUT
-            if(not show_processed_image): continue
+            if(not flags['show_processed_image']): continue
 
             cv2.imwrite(
             '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
@@ -76,19 +81,62 @@ def get_flags(flags):
         "is_single_file" : IS_SINGLE_FILE,
         "output_name_file" : OUTPUT_NAME,
         "output_type" : DEFAULT_OUT_TYPE, #MAKE ENUM HERE
+        "show_processed_image" : SHOW_PROCESSED_IMAGE,
+        "show_console_log" : SHOW_CONSOLE_LOG,
     }
 
-    if('-s' in flags): flags_obj['is_single_file'] = True
+    if('-s' in flags): flags_obj['is_single_file'] = IS_SINGLE_FILE
     if('-d' in flags): flags_obj['dir_path'] = flags[flags.index("-d")+1]
-    if('-o' in flags): flags_obj['output_name_file'] = flags[flags.index("-d")+1]
+    if('-o' in flags): flags_obj['output_name_file'] = flags[flags.index("-o")+1]
+    if('-i' in flags): flags_obj['show_processed_image'] = True
+    if('-c' in flags): flags_obj['show_console_log'] = True
     
     return flags_obj
+
+def save_landmarks(landmarks, output_file):
+    for landmark in landmarks.landmark:
+        print("LANDMARK: ", landmark)
+        output_file.write("{},".format(str(landmark.x)))
+        output_file.write("{},".format(str(landmark.y)))
+        output_file.write(str(landmark.z))
+    output_file.write("\n")
+
+
+def read_all_images(path):
+    #POR DEFAULT OS CAMINHO TEM QUE SEMPRE INCLUIR /
+    label = path.split('/')[-1]
+    images = list(filter(lambda f: os.path.isfile(os.path.join(path+'/', f)) ,os.listdir(path+'/')))
+    if(len(images) == 0):
+        print("(I) NO IMAGES IN: {}".format(path))
+    else:
+        print("(I) label ({}) -> READING IMAGES IN {}: {}".format(label, path, images))
+        
+
+    folders = list(filter(lambda f: os.path.isdir(os.path.join(path+'/', f)) ,os.listdir(path+'/')))
+    print("(F) READING FOLDERS IN {}: {}".format(path, folders))
+
+    for folder in folders:
+        read_all_images(os.path.join(path+'/', folder))
+
+
+
+
+
+def train(input_file) :
+    print("==================( CNN Training (START) )==================")
+    #TODO
+    print("==================( CNN Training   (END) )==================")
+    
 
 
 def main():
     flags = get_flags(sys.argv)
-    print(flags)
-    get_landmarks(flags["dir_path"])
+    print("Provided Flags: "+str(flags))
+    if(os.path.exists(flags['output_name_file'])): os.remove(flags['output_name_file'])
+    output_file = open(flags['output_name_file'], 'a')
+
+    read_all_images(flags["dir_path"])
+    #get_landmarks(flags["dir_path"], output_file, flags)
 
 
 
