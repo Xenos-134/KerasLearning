4af8dec788433b033ed2813262f380b5bfdd4e69
^ↀᴥↀ^ <87715226+Xenos-134@users.noreply.github.com>
2022-07-03 17:20:38 +0100

    Added: get_model to train.py

--- train.py
+++ train.py
@@ -4,6 +4,7 @@
 #==============================================================#
 
 from cProfile import label
+from http.client import TEMPORARY_REDIRECT
 from statistics import mode
 import sys
 from tabnanny import verbose
@@ -21,12 +22,7 @@ from sklearn.preprocessing import LabelBinarizer
 encoder = LabelBinarizer()
 
 
-def train(input_file) :
-    print("==================( CNN Training (START) )==================")
-    #TODO
-    print("==================( CNN Training   (END) )==================")
-
-
+TEMPORARY_MODEL_NAME = "hands_model"
 
 #==============================================================#
 #                       TEST PART OF CNN                       #                                        
@@ -48,66 +44,68 @@ def test_train2():
         labels_dataset.append(element.decode().replace(",", ""))
 
     labels_dataset = generate_labels(labels_dataset)
-    print(labels_dataset)
+    #print(labels_dataset)
 
-    #print(train_dataset)
+    print(len(train_dataset))
    
     model = models.Sequential()
-    model.add(layers.Flatten(input_shape=(63,)))
-    model.add(layers.Dense(128, activation='relu'))
-    model.add(layers.Dropout(0.2))
-    model.add(layers.Dense(10))
+    model.add(layers.Dense(32, input_shape=(63, ) ,activation='relu')) #Os neuronios 'e aleatorio
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(3, activation='softmax'))
 
     model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-    #model.summary()
+    model.summary()
+    print(labels_dataset)
 
-    history = model.fit(train_dataset, labels_dataset, epochs=10, 
+    history = model.fit(train_dataset, labels_dataset, epochs=30, 
                         validation_data=(train_dataset, labels_dataset))
-
+                        
     print("==================== [ FINISHED ]==========================")
     test_loss, test_acc = model.evaluate(train_dataset, labels_dataset, verbose=2)
     print("===========================================================")
 
-    #save_model(model, "test_model")
+    save_model(model, TEMPORARY_MODEL_NAME)
+    #print(model.predict([train_dataset[-15]]))
 
-labels = ["A", "B", "C", "D", "E", "F"]
+labels = ["A", "B", "C"]
 
 
 def generate_labels(train_labels):
     new_labels_list = []
     for label in train_labels:
         new_labels_list.append(labels.index(label))
-
     return new_labels_list
     
 
-
 def save_model(model, model_name):
-    #TODO
-    print("TEST")
+    print("Saved Model: ", model_name)
     model.save("./"+model_name)
     save_wights(model, model_name)
 
 
 def save_wights(model, file_name):
+    print("Saved Weights: ", file_name)
     model.save_weights('./'+file_name+'.h5')
 
-def load_model(file_name):
-    print("TEST")
-    model = tf.keras.models.load("./"+file_name)
 
-
-def load_weights(model, file_name):
-    model.load_wights(file_name+'.h5')
-
+def get_model(file_name):
+    print("Loading model and weights: ", file_name)
+    model = tf.keras.models.load_model("./"+file_name)
+    model.load_weights('./'+file_name+'.h5')
+    #model.summary()
+    #print(model.get_weights())
+    return model
 
 def main():
     #flags = sys.argv
     #input_file = open(sys.argv, 'r')
     #input_file.close() 
     test_train2()
+
+    #loaded_model = get_model(TEMPORARY_MODEL_NAME)
+
 
 main()
