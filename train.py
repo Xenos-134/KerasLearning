cbc5f52deb2bbfc423561515fa1528c638e91ee1
^ↀᴥↀ^ <87715226+Xenos-134@users.noreply.github.com>
2022-07-04 15:07:13 +0100

    Addded: Show plt

--- train.py
+++ train.py
@@ -20,6 +20,14 @@ from tensorflow.keras.optimizers import Adam
 import numpy as np
 from sklearn.preprocessing import LabelBinarizer
 from hands_data_extraction import * 
+from sklearn.model_selection import train_test_split
+
+from sklearn.metrics import confusion_matrix
+import itertools
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 encoder = LabelBinarizer()
 
@@ -48,6 +56,11 @@ def test_train2():
     labels_dataset = generate_labels(labels_dataset)
     #print(labels_dataset)
 
+    #SPLIT TO TRAIN
+    train_dataset, t_train_dataset, labels_dataset, t_labels_dataset = train_test_split(train_dataset, labels_dataset, test_size=0.1)
+
+
+    
     print("==================== [ START ]==========================")
    
     model = models.Sequential()
@@ -69,10 +82,61 @@ def test_train2():
     test_loss, test_acc = model.evaluate(train_dataset, labels_dataset, verbose=2)
     print("===========================================================")
 
-    save_model(model, TEMPORARY_MODEL_NAME)
-    #print(model.predict([train_dataset[-15]]))
+    #save_model(model, TEMPORARY_MODEL_NAME)
+
+    #MAKING PREDICTION TO GENERATE CONFUSION MATRIX
+    predictions = model.predict(t_train_dataset)
+    #rounded_predictions = np.argmax(predictions)
+    rounded_predictions = []
+    for elm in predictions:
+        rounded_predictions.append(np.argmax(elm))
+
+    print(len(rounded_predictions))
+    print("\n\n\n")
+    print(len(t_labels_dataset))
+    cm = confusion_matrix(y_true=t_labels_dataset, y_pred=rounded_predictions)
+    plot_confusion_matrix(cm, labels)
+
 
 labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    df_cm = pd.DataFrame(cm, index = [i for i in labels],
+                  columns = [i for i in labels])
+    plt.figure(figsize = (15,15))
+    #sn.heatmap(df_cm, annot=True)
+    plt.show()
 
 
 def generate_labels(train_labels):
@@ -114,10 +178,10 @@ def main():
     #flags = sys.argv
     #input_file = open(sys.argv, 'r')
     #input_file.close() 
-    #test_train2()
+    test_train2()
 
-    loaded_model = get_model(TEMPORARY_MODEL_NAME)
-    predict("test.jpg", loaded_model)
+    #loaded_model = get_model(TEMPORARY_MODEL_NAME)
+    #predict("test.jpg", loaded_model)
 
 
 main()
